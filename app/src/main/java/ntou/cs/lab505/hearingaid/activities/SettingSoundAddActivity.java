3c41bd94e2d95573ad808bd0aa18fdb7a0de808e
alantitor <belosianau@gmail.com>
2015-02-23 21:41:28 +0800

    finish sound add activity layout

--- app/src/main/java/ntou/cs/lab505/hearingaid/activities/SettingSoundAddActivity.java
+++ app/src/main/java/ntou/cs/lab505/hearingaid/activities/SettingSoundAddActivity.java
@@ -1,18 +1,96 @@
 package ntou.cs.lab505.hearingaid.activities;
 
 import android.app.Activity;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.SeekBar;
+import android.widget.Toast;
 
 import ntou.cs.lab505.hearingaid.R;
+import ntou.cs.lab505.hearingaid.sqlite.DoSqlite;
+import ntou.cs.lab505.hearingaid.sqlite.TableContract;
 
-public class SettingSoundAddActivity extends Activity {
+public class SettingSoundAddActivity extends Activity implements SeekBar.OnSeekBarChangeListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting_sound_add);
+
+        // ...
+
+        SeekBar sb = (SeekBar) findViewById(R.id.setting_freq_band_seekBar);
+        sb.setOnSeekBarChangeListener(this);
+    }
+
+    /**
+     * Reload data to view from database
+     */
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        // ...
+    }
+
+    /**
+     * save data to database
+     */
+    @Override
+    public void onPause() {
+
+        // ...
+
+        super.onPause();
+    }
+
+    /**
+     *
+     * @param seekBar
+     * @param progress
+     * @param fromUser
+     */
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+    }
+
+    /**
+     *
+     * @param seekBar
+     */
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+        //
+    }
+
+    /**
+     *
+     * @param seekBar
+     */
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+        //
+    }
+
+    /**
+     *
+     * @param message
+     */
+    public void alertMessage(String message) {
+        Context context = getApplicationContext();
+        CharSequence text = message;
+        int duration = Toast.LENGTH_LONG;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
 }
---
+++ app/src/main/java/ntou/cs/lab505/hearingaid/debug/Debug.java
@@ -0,0 +1,12 @@
+package ntou.cs.lab505.hearingaid.debug;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * Created by alan on 2015/2/15.
+ */
+public class Debug {
+
+
+}
